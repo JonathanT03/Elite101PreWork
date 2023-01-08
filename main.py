55868d2443a3546954e95a7865197de837b67005
Jonathan Torres <1jonathan.t.03@gmail.com>
2023-01-08 03:01:57 +0000

    code

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import math
 import random 
+# All responses or using the random module#
+greeting = random.choice(["Hello there, what is your name?", "Greetings, Haven't talked to anyone in a while. What is your name?", "Nice to Meet you, can you tell me your name?"])
+
+
+###############################################################
+
+# Start of program ##
+name = input((greeting))
+print(f' \n Hello there {name}, I am Spark, a bot that enjoys talking to humans!')
+
+#Guessing Game##
